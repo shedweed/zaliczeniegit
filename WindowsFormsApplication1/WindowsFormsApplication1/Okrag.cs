cd747a01383ff18df19118db060671fabd2762da
Marek <zemi.katalog@gmail.com>
2014-06-21 14:00:51 +0200

    Dodano klase Okrag do obliczania pola okregu

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Okrag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    public class Okrag
+    {
+        public double poleOkregu(double r)
+        {
+            return 2* 3.14 * r;
+        }
+    }
+}
