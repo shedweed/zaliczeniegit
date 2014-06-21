5493e1e637db63b09ef784cbbe7d7f9c857162ca
Marek <zemi.katalog@gmail.com>
2014-06-21 13:59:11 +0200

    Dodano klase Prostokat - do obliczania pola prostokatu

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Prostokat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class Prostokat
+    {
+        public int poleProstokatu(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
